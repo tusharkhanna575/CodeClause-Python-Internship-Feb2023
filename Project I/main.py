f756d7dba678d0f80b6acca465ee88f280e81b45
Tushar Khanna <0808cs201190.ies@ipsacademy.org>
2023-02-04 19:57:40 +0530

    Update main.py

--- Project I/main.py
+++ Project I/main.py
@@ -1,1 +1,15 @@
-pass
+# code by @tusharkhanna
+
+import string
+import random
+import streamlit as st
+
+def generate_password(length):
+    chars = string.ascii_letters + string.digits + string.punctuation
+    password = ''.join(random.choice(chars) for i in range(length))
+    return password
+
+st.title("Random Password Generator")
+password_length = st.slider("Password Length", min_value=8, max_value=32, value=16, step=1)
+generated_password = generate_password(password_length)
+st.write("Generated Password:", generated_password)
